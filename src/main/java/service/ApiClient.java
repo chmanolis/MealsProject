90538ed669185512453a93d599d603685eba76bf
chmanolis <chmanolis91@gmail.com>
2023-02-19 22:10:18 +0200

    add some urls

--- src/main/java/service/ApiClient.java
+++ src/main/java/service/ApiClient.java
@@ -16,6 +16,15 @@ public class ApiClient {
     public static final String SEARCH_BY_NAME_URL = BASE_URL+"search.php?s=";
     public static final String SEARCH_BY_FIRST_LETTER_URL = BASE_URL+"search.php?f=";
     public static final String FULL_MEAL_DETAILS_URL = BASE_URL+"lookup.php?i=";
+    public static final String RANDOM_MEAL_URL = BASE_URL+"random.php";
+    public static final String MEAL_CATEGORIES_URL = BASE_URL+"categories.php";
+
+    public static final String LIST_BY_CATEGORY = BASE_URL+"list.php?c=list";
+    public static final String LIST_BY_AREA  = BASE_URL+"list.php?a=list";
+    public static final String  LIST_BY_INGREDIENTS= BASE_URL+"list.php?i=list";
+    public static final String FILTER_BY_MAIN_INGREDIENT = BASE_URL+"filter.php?i=";
+    public static final String FILTER_BY_MAIN_CATEGORY = BASE_URL+"filter.php?c=";
+    public static final String FILTER_BY_MAIN_AREA = BASE_URL+"filter.php?a=";
 
 
 
