c91b821a29d88ec8614416d7da3ad3387f900ce8
chmanolis <chmanolis91@gmail.com>
2023-02-23 17:33:35 +0200

    Start with GUI

---
+++ src/main/java/gui/UserMenu.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+public class UserMenu {
+    private JPanel panel1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+
+
+    }
+
+
